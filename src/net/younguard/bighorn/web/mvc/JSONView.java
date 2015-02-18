de16d37a792895516210e6976d8643125327d533
thomas <thomas.zh@qq.com>
2015-02-18 21:26:31 +0800

    init html page

--- src/net/younguard/bighorn/web/mvc/JSONView.java
+++ src/net/younguard/bighorn/web/mvc/JSONView.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.view.AbstractView;
 
+import com.google.gson.Gson;
+
 /**
  * this view writes the json response to the httpResponse
  * 
@@ -22,25 +24,26 @@ public class JSONView
 		setContentType("application/json");
 	}
 
-	protected void renderMergedOutputModel(Map model,
-			HttpServletRequest request, HttpServletResponse response)
+	protected void renderMergedOutputModel(Map model, HttpServletRequest request, HttpServletResponse response)
 			throws Exception
 	{
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
 
 		response.setContentType(getContentType());
-		String rs = JSONSerializer.toJSON(model.get("JSON_OBJECT")).toString();
-		////System.out.println(rs);
+		Gson gson = new Gson();
+		String rs = gson.toJson(model.get("JSON_OBJECT"));
+
+		// //System.out.println(rs);
 		// the JSONSerializer is part of - http://json-lib.sourceforge.net/, we
 		// get the json array from the model hashmap, let the serializer turn it
 		// into json text, then we send it down the wire
-		//response.getWriter().flush();
+		// response.getWriter().flush();
 		response.getWriter().write(rs);
-		//response.getWriter().
-		
-		//tmp add code by andy
-		//response.sendRedirect("view.html");
-		//response.sendRedirect("catalog-query.go");
+		// response.getWriter().
+
+		// tmp add code by andy
+		// response.sendRedirect("view.html");
+		// response.sendRedirect("catalog-query.go");
 	}
 }
